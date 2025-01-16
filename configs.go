8bac3ab54b9f3b88b28d71c776dfc69b402709f6
Rafsan Uddin Beg Rizan <rafsan@bloxley.com>
2025-01-16 21:13:00 +0600

    remove redundant

--- configs.go
+++ configs.go
@@ -6,31 +6,11 @@ type AppConfig struct {
 }
 
 // Config holds all required environment variables.
-type Config struct {
+type DBConfig struct {
 	DBUser  string
 	DBPass  string
 	DBHost  string
 	DBPort  string
 	DBName  string
 	SSLMode string
-}
-
-// LoadDBConfig loads the application configuration from environment variables.
-func LoadDBConfig() *Config {
-	return &Config{
-		DBUser:  GetEnvOrFail(DBUserKey),
-		DBPass:  GetEnvOrFail(DBPassKey),
-		DBHost:  GetEnvOrFail(DBHostKey),
-		DBPort:  GetEnvOrFail(DBPortKey),
-		DBName:  GetEnvOrFail(DBNameKey),
-		SSLMode: GetEnvOrFail(SSLModeKey),
-	}
-}
-
-// LoadAppConfig loads the application configuration from environment variables.
-func LoadAppConfig() *AppConfig {
-	return &AppConfig{
-		Port: GetEnvOrFail(AppPortKey),
-		Env:  GetEnvOrFail(EnvKey),
-	}
 }
