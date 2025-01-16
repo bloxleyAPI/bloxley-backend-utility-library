90b3b5baac071971370af4fa3cbe92b3e9c7eca0
Rafsan Uddin Beg Rizan <rafsan@bloxley.com>
2025-01-16 20:04:25 +0600

    added root file

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// This root file is must or other Microservices will not be able to call this package
+func main() {
+	// This is the main function
+	fmt.Println("Hello, World!")
+}
