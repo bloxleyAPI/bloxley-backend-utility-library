31af265eaf8b73f3239bebde4096f3841a4724c4
Rafsan Uddin Beg Rizan <rafsan@bloxley.com>
2025-01-30 21:59:36 +0600

    env value as int

--- get_env.go
+++ get_env.go
@@ -3,6 +3,7 @@ package utilslibs
 import (
 	"log"
 	"os"
+	"strconv"
 )
 
 // getEnvOrFail is a helper that crashes if an env variable is missing or empty.
@@ -12,4 +13,21 @@ func GetEnvOrFail(key string) string {
 		log.Fatalf("missing required environment variable: %s", key)
 	}
 	return val
+}
+
+// getIntEnvOrFail is a helper that crashes if an env variable is missing or empty.
+func GetIntEnvOrFail(key string) int {
+	val, ok := os.LookupEnv(key)
+	if !ok || val == "" {
+		log.Fatalf("missing required environment variable: %s", key)
+	}
+
+	// Convert string to int
+	intVal, err := strconv.Atoi(val)
+
+	if err != nil {
+		log.Fatalf("failed to convert environment variable %s to int", key)
+	}
+
+	return intVal
 }
