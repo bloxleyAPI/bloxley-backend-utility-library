f20d153a9cab6f65ebdb808735322b788551a5e7
Rafsan Uddin Beg Rizan <rafsan@bloxley.com>
2025-02-14 20:20:17 +0600

    bool helper

--- helpers.go
+++ helpers.go
@@ -13,6 +13,11 @@ func StringToPointer(s string) *string {
 	return &s
 }
 
+// Bool to pointer
+func BoolToPointer(b bool) *bool {
+	return &b
+}
+
 // Contains returns true if slice s contains the element e.
 func Contains[T comparable](s []T, e T) bool {
 	for _, v := range s {
